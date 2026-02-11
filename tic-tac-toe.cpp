fb04e12c667e8eb3ec14cc514895a97507bf95c1
M4HiN <mahin.mr009@gmail.com>
2026-02-12 00:55:56 +0600

    Re-Structured Logic & Linear going bot added.

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,99 +1,154 @@
-#include <bits/stdc++.h>
+#include<iostream>
 #include<ctime>
+#include<vector>
+#include<utility>
+//#include<random>
 using namespace std;
-void printDiagram(char arr[3][3]);
-int status(char arr[3][3]);
 
-int main(void) {
-   cout<<"***************Tic-Tac-Toe********************\n";
-   char arr[3][3]={ {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };
-   int rowValues[3]={-1,-1,-1},colValues[3]={-1,-1,-1}; // i have to work here for taking input for computer
+vector <pair<int,int>> all_pos = {{0,0},{0,1},{0,2},
+                                  {1,0},{1,1},{1,2},
+                                  {2,0},{2,1},{2,2} };
 
-   char player = 'u'; bool computer = false;
-
- cout<<"If you want to play with computer input 'y'/'Y' else 'n'/'N' to play with friend.\n";
-   cin>>player;
-   if(player>Z) player = player - 32;
-   if(player == 'N' || player == 'Y') computer = true;
-   else {cout<<"INVALID player type chosen! re-run the program!\n"; return 0;}
-   
-
+pair<int,int> bot(vector <pair<int,int>> &stored_pos){
     
+   for(auto a_pos : all_pos) {
+    bool found = false;
+        for(auto it: stored_pos) {
+            if(it.first == a_pos.first && it.second == a_pos.second)
+                {found = true; break; }
+        }
+       if(!found)
+        return {a_pos.first,a_pos.second};  
+  }  
+  if(stored_pos.size() == 9) return {-2,-2};
+   cout<<"An Error Occured in BOT section!!\n";
+   return {-1,-1};
 
-   printDiagram(arr);
-   int turns = 9;
-   int stat = -1;
-   while(turns--){
-       int row,col; char cellValue;
-       cout<<"Enter Position Row(1-3) and Column (1-3)\n";
-       cout<<"Row: ";cin>>row; cout<<endl;
-       cout<<"Column: ";cin>>col; cout<<endl;
-     
-       if(row<1 || col<1 || col>3 || row > 3){
-         cout<<"You have exceeded the boundary of the set! Now re-run the program!!\n";
-         return 0;
-       }
+}
 
-       cout<<"Enter X or O: "; cin>>cellValue; cout<<endl;
-       arr[row-1][col-1] = cellValue;
-       printDiagram(arr);
-       stat = status(arr);
-       if(stat)
-       {cout<<arr[row-1][col-1]<<" WINS!!! \n";break;}
-       
-   }
-   if(stat == -1) {
-      cout<<"UNEXPECTED ERROR! Re-run the program.\n"
-   }
-   if(!stat) cout<<"DRAW!\n";
-   
-   
-   
+void printDiagram( char (*arr)[3]);
+bool checkStatus(char (*arr)[3],char p1,char p2);
+void greet(int p);
+int main() {
  
+    char grid[3][3] = {0};
+    vector <pair<int,int>> stored_pos;
+    pair <int,int> botVal;
+
+    char p1,p2,ChM; bool turn = true, machine = true;
+    
+  cout<<"Play with BOT? Enter Y/y, else N/n to play with friend"<<endl;
+  cin>>ChM;
+  if(ChM =='N' || ChM == 'n')
+    machine = false;
+
+
+      cout<<"Choose X or O for player 1: ";
+      cin>> p1;
+       
+      p1 == 'X' ? p2='O':p2='X';
+
+            for(int i=0;i<9;i++){ 
+                short int r = -1, c = -1;
+            if(turn) cout<<"Player 1 turn "<<endl;
+            else cout<<"Player 2 turn "<<endl;
+
+            cout << "Enter Row and Column :";
+            cin >> r >> c; cout<<endl;
+                
+                if(r<1 || c<1 || r>3 || c>3){
+                
+                cout<<"Invalid row and column"<<endl;
+                return 0;
+                }
+
+                stored_pos.push_back({r-1,c-1});
+                
+
+                          
+                      if(turn) 
+                         grid[r-1][c-1] = p1;
+                      
+                      else 
+                         grid[r-1][c-1] = p2;
+                      
+                        
+                         if(machine)
+                             {
+                              botVal = bot(stored_pos);
+                              stored_pos.push_back(botVal);
+                               
+                              if(botVal.first == -1) {
+                                return 0;  
+                  
+                              }
+
+                              grid[botVal.first][botVal.second] = 'O';
+                              }
+                          
+                          else 
+                            turn = !turn;
+
+         
+           printDiagram(grid);
+                 //if(i>3) 
+                    if(checkStatus(grid,p1,p2)) return 0;
+                 }
+          
+          
+           greet(0);
+            return 0;
+    
 }
 
-void printDiagram(char arr[3][3]){
-   cout<<"      |       |       \n";
-   cout<<"  "<<arr[0][0]<<"   |   "<<arr[0][1]<<"   |   "<<arr[0][2]<<endl;
-   cout<<"      |       |       \n";
-   cout<<"----------------------\n";
-   cout<<"      |       |       \n";
-   cout<<"  "<<arr[1][0]<<"   |   "<<arr[1][1]<<"   |   "<<arr[1][2]<<endl;
-   cout<<"      |       |       \n";
-   cout<<"----------------------\n";
-   cout<<"      |       |       \n";
-   cout<<"  "<<arr[2][0]<<"   |   "<<arr[2][1]<<"   |   "<<arr[2][2]<<endl;
-   cout<<"      |       |       \n";
+
+void printDiagram(char (*arr)[3]){
+  
+    for(int i=0;i<3;i++){
+          cout<<arr[i][0]<<" | "<<arr[i][1]<<" | "<<arr[i][2]<<endl;
+          if(i!=2)
+          cout<<" - "<<" - "<<" - "<<endl;
+    }
+
+return;
 }
 
-int status(char arr[3][3]){
-    int win=0;
-   if((arr[0][0]==arr[0][1] && arr[0][0]==arr[0][2])&&(arr[0][2]=='X' || arr[0][2]=='O'))
-   win = 1;
-   else if((arr[1][0]==arr[1][1] && arr[1][1]==arr[1][2])&&(arr[1][2]=='X' || arr[1][2]=='O'))
-   win = 1;
-   else if((arr[2][0]==arr[2][1] && arr[2][1]==arr[2][2])&&(arr[2][2]=='X' || arr[2][2]=='O'))
-   win = 1;
-   else if((arr[0][0]==arr[1][0] && arr[1][0]==arr[2][0])&&(arr[2][0]=='X' || arr[2][0]=='O'))
-   win = 1;
-   else if((arr[0][1]==arr[1][1] && arr[0][1]==arr[2][1])&&(arr[2][1]=='X' || arr[2][1]=='O'))
-   win = 1;
-   else if((arr[0][2]==arr[1][2] && arr[0][2]==arr[2][2])&&(arr[2][2]=='X' || arr[2][2]=='O'))
-   win = 1;
-   else if((arr[0][0]==arr[1][1] && arr[1][1]==arr[2][2])&&(arr[2][2]=='X' || arr[2][2]=='O'))
-   win = 1;
-   else if((arr[0][2]==arr[1][1] && arr[1][1]==arr[2][0])&&(arr[2][0]=='X' || arr[2][0]=='O'))
-   win = 1;
-   
-   if (win) return 1;
-   return 0;
-   
+bool checkStatus (char (*arr)[3],char p1,char p2){
+  int sum[8] = {0};
+
+  for(int i=0;i<3;i++){
+    sum[0]+= (int)arr[0][i];
+    sum[1]+= (int)arr[1][i];
+    sum[2]+= (int)arr[2][i];
+    sum[3]+= (int)arr[i][0];
+    sum[4]+= (int)arr[i][1];
+    sum[5]+= (int)arr[i][2];
+    sum[6]+= (int)arr[i][i];
+    sum[7]+= (int)arr[i][2-i];
+  }  
+  for(int i=0;i<8;i++){
+  
+    if(sum[i]== (int) 3*p1) 
+     {  greet(1); return true;}
+    else if(sum[i]== (int)3*p2) 
+     { greet(2); return true;}
+
+  }
+  return false;
+
 }
+void greet(int p){
+    
+    if(p==0) 
+    {cout<<"   Draw!  "<<endl; return;}
 
-void autoInsert(arr[3][3],int rowValues[3],int colValues[3]){
-   srand(time(NULL)); 
-   int row = rand() % 3 + 1 , col = rand() % 3 + 1;
-   while()
-
-}   
-
+    for(int j=0;j<5;j++){
+        for(int i=0;i<20;i++)
+        { 
+         if(j==2 && i==1) cout<<" Player "<<p<<" Wins!!! ";
+         else if(j==0 || j==4 ||i==0||i==19) cout<<'*';
+         else if( !(j==2) ) cout<<' ';
+        }
+         cout<<endl;
+}
+}
